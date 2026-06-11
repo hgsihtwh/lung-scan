0000f2ebfe34115748297ca9ce5d4356efb6bf0c
hgsihtwh <hgsihtwh@gmail.com>
2026-06-11 16:44:09 +0700

    chore: экспорт DoctorPage и AdminPage

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,2 +1,4 @@
 export { default as AuthPage } from './AuthPage'
 export { default as ResetPasswordPage } from './ResetPasswordPage'
+export { default as DoctorPage } from './DoctorPage'
+export { default as AdminPage } from './AdminPage'
